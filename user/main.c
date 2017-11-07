c6d2d4fe98abf49a2b63248ad5fad009c9344cf1
714826565 <714826565@qq.com>
2017-11-07 16:28:44 +0800

    创建一个任务，但为测试是否可以运行

--- user/main.c
+++ user/main.c
@@ -1,11 +1,15 @@
 #include "stm32f10x.h"
 #include "led.h"
+#include <ucos_ii.h>
 
 void delay(u32 i)
 {
 	while (i--);
 }
 
+static OS_STK Task_led_STK[OS_TASK_STAT_STK_SIZE];
+
+void Task_led (void *p_arg);
 /**
   * @brief  Main program.
   * @param  None
@@ -13,9 +17,17 @@ void delay(u32 i)
   */
 int main(void)
 {
-	LED_gpio_init();
+	OSInit();
+    OSTaskCreate(Task_led, (void*)0, &Task_led_STK[OS_TASK_STAT_STK_SIZE - 1], 0);
+	OSStart();
 
-	while (1)
+	return 0;
+}
+
+void Task_led (void *p_arg)
+{
+    LED_gpio_init();
+    while (1)
 	{
 		static unsigned char i = 0;
 		LED_GPIO_set((i % 8), 1);
@@ -24,6 +36,4 @@ int main(void)
 		delay(6000000);
 		i++;
 	}
-
-	return 0;
 }
